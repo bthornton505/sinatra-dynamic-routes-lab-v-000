0c34ae0ff147de94138a7dff3e3fd84643daa489
Brenden Thornton <thorntonbrenden@gmail.com>
2018-09-10 23:42:42 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -28,8 +28,7 @@ class App < Sinatra::Base
   end 
   
   get '/:operation/:number1/:number2' do 
-    @product = params[:number1].to_i params[:operation] params[:number2].to_i
-    "#{@product}"
+    
   end 
   
 end
